42885ae614e5ef8eb69d0cb5eade63d491c40575
unknown <chernandez@VMDevTM001.cgi.clayton.com>
2013-08-20 13:37:26 -0400

    Including AUTH using angularfire simple login

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,20 @@
 
 /* Controllers */
 
+var fireBaseUrl = 'https://hercax.firebaseio.com/';
+
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+	controller('MainCtrl', ['$scope', 'angularFire', function($scope, angularFire) {
 
-  }])
-  .controller('MyCtrl2', [function() {
+	}]).
+	controller('NavCtrl', ['$scope', 'angularFire', 'angularFireAuth', function($scope, angularFire, angularFireAuth) {
+		angularFireAuth.initialize(fireBaseUrl, {scope: $scope, name: "user"});
+		
+		$scope.login = function() {
+			angularFireAuth.login("facebook");
+		};
 
-  }]);
+		$scope.logout = function() {
+			angularFireAuth.logout();
+		};
+	}]);
