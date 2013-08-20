83b507e430214b35db18fa0da157cc6a0dac7502
unknown <chernandez@VMDevTM001.cgi.clayton.com>
2013-08-20 17:16:28 -0400

    Changes to Services

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,17 +5,21 @@
 var fireBaseUrl = 'https://hercax.firebaseio.com/';
 
 angular.module('myApp.controllers', []).
-	controller('MainCtrl', ['$scope', 'angularFire', function($scope, angularFire) {
+	controller('MainCtrl', ['$scope', 'chatService', 'angularFire', function($scope, chatService, angularFire) {
 
 	}]).
-	controller('NavCtrl', ['$scope', 'angularFire', 'angularFireAuth', function($scope, angularFire, angularFireAuth) {
+	controller('NavCtrl', ['$scope', 'chatService', 'angularFire', 'angularFireAuth', function($scope, chatService, angularFire, angularFireAuth) {
 		angularFireAuth.initialize(fireBaseUrl, {scope: $scope, name: "user"});
-		
+
 		$scope.login = function() {
-			angularFireAuth.login("facebook");
+			angularFireAuth.login("facebook", {scope: 'email, user_likes, read_friendlists'}).then(function (){
+				chatService.user = $scope.user;
+			});
 		};
 
 		$scope.logout = function() {
-			angularFireAuth.logout();
+			angularFireAuth.logout().then(function() {
+				chatService.user = $scope.user;
+			});
 		};
 	}]);
