96a0abca5d68e41cf6253b078726572f94f70a3e
Carlos Hernandez <hercax@gmail.com>
2013-08-20 23:40:34 -0400

    Added ChatService

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,20 +6,21 @@ var fireBaseUrl = 'https://hercax.firebaseio.com/';
 
 angular.module('myApp.controllers', []).
 	controller('MainCtrl', ['$scope', 'chatService', 'angularFire', function($scope, chatService, angularFire) {
+		$scope.chatService = chatService;
+		$scope.user = function () { return chatService.user };
 
 	}]).
 	controller('NavCtrl', ['$scope', 'chatService', 'angularFire', 'angularFireAuth', function($scope, chatService, angularFire, angularFireAuth) {
-		angularFireAuth.initialize(fireBaseUrl, {scope: $scope, name: "user"});
+		$scope.chatService = chatService;
+		$scope.user = function () { return chatService.user };
+
+		angularFireAuth.initialize(fireBaseUrl, {scope: $scope, name: "chatService.user"});
 
 		$scope.login = function() {
-			angularFireAuth.login("facebook", {scope: 'email, user_likes, read_friendlists'}).then(function (){
-				chatService.user = $scope.user;
-			});
+			angularFireAuth.login("facebook", {scope: 'email, user_likes, read_friendlists'});
 		};
 
 		$scope.logout = function() {
-			angularFireAuth.logout().then(function() {
-				chatService.user = $scope.user;
-			});
+			angularFireAuth.logout();
 		};
 	}]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,9 +5,8 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).factory('chatService', function($rootScope){
+angular.module('myApp.services', []).factory('chatService', ['$rootScope', function($rootScope){
 	var chatService = {};
 
-	chatService.user = null;
-	chatService.messages = [];
-});
+	return chatService;
+}]);
